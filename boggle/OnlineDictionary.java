d1dc5f1010b040fb47b0ad1e2388cc3f9b3ee76e
gideonmehna <63281826+gideonmehna@users.noreply.github.com>
2022-12-04 14:21:55 -0500

    Online Dictionary Under Construction

--- boggle/OnlineDictionary.java
+++ boggle/OnlineDictionary.java
@@ -1,11 +1,6 @@
 package boggle;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Objects;
-import java.util.TreeSet;
+import java.util.HashMap;
 
 /**
  * The Dictionary class for the first Assignment in CSC207, Fall 2022
@@ -18,57 +13,123 @@ public class OnlineDictionary {
      *  we will load the words depeding on the users preferences.
      *  but once a word is loaded, we want to temporariyl keep the word until the round is ended.
      */
-    private TreeSet<String> fetchedWords;
-    private String word;
+    private HashMap<String, String> fetchedWords;
+
+    private int wordCount;
 
     /**
      * Class constructor
      *
-     * @param word the file containing a list of legal words.
+     * This initializes the Online Dictionary.
      */
-    public OnlineDictionary(String word) {
-        String line = "";
-        int wordcount = 0;
-        this.fetchedWords = new TreeSet<String>();
+    public OnlineDictionary() {
+        this.wordCount = 0;
+        this.fetchedWords = new HashMap<String, String>();
 
-        System.out.println("Initialized " + wordcount + " words in the Dictionary.");;
     }
 
     /* 
-     * Checks to see if a provided word is in the dictionary.
+     * Checks to see the meaning of the provided word and adds the meaning to the dictionary
      *
      * @param word  The word to check
-     * @return  A boolean indicating if the word has been found
+     * @return  The meaning of the word found
      */
-    public boolean containsWord(String word) {
-        // looping through the Treeset
-        for(String s: this.fetchedWords){
-            // Checking if the word equals a word s in the treeset
-            if (Objects.equals(word.toLowerCase(), s)){
-                return true;
-            }
-        }
-        return false;
+    public String fetchWordMeaning(String word) {
+        // Check if the word is already in the dictionary
+        // fetch the meanig of the word
+        // Create a neat value object to hold the URL
+
+
+//        URL url = new URL("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+//
+//// Open a connection(?) on the URL(??) and cast the response(???)
+//        HttpURLConnection connection = null;
+//        try {
+//            connection = (HttpURLConnection) url.openConnection();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//// Now it's "open", we can set the request method, headers etc.
+//        connection.setRequestProperty("accept", "application/json");
+//
+//// This line makes the request
+//        InputStream responseStream = null;
+//        try {
+//            responseStream = connection.getInputStream();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//// Manually converting the response body InputStream to APOD using Jackson
+//        ObjectMapper mapper = new ObjectMapper();
+//        APOD apod = mapper.readValue(responseStream, APOD.class);
+//
+//// Finally we have the response
+//        System.out.println(apod.title);
+//
+
+
+
+
+        // Fake example transaction ID: 3YC00XQKNVMZ
+//        String url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+//        Header headerKey = new Header("X-Kount-Api-Key", "<!--Actual Kount RIS/API Key goes here-->");
+//
+//        // Create an instance of HttpClient.
+//        HttpClient client = new HttpClient();
+//
+//        // Create a method instance.
+//        GetMethod method = new GetMethod(url);
+//
+//        // Provide custom retry handler is necessary
+//        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+//                new DefaultHttpMethodRetryHandler(3, false));
+//        method.addRequestHeader(headerKey);
+//
+//        try {
+//            // Execute the method.
+//            int statusCode = client.executeMethod(method);
+//
+//            if (statusCode != HttpStatus.SC_OK) {
+//                System.err.println("Method failed: " + method.getStatusLine());
+//            }
+//
+//
+//            // Read the response body.
+//            byte[] responseBody = method.getResponseBody();
+//
+//            // Deal with the response.
+//            // Use caution: ensure correct character encoding and is not binary data
+//            System.out.println(new String(responseBody));
+//
+//        } catch (HttpException e) {
+//            System.err.println("Fatal protocol violation: " + e.getMessage());
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            System.err.println("Fatal transport error: " + e.getMessage());
+//            e.printStackTrace();
+//        } finally {
+//            // Release the connection.
+//            method.releaseConnection();
+//        }
+//    }
+        //add the meaning of the word
+        // increase the word count
+        // return the meaning of the word
+ throw new UnsupportedOperationException();
     }
 
-    /* 
-     * Checks to see if a provided string is a prefix of any word in the dictionary.
+    /*
+     * Checks the number of successful API calls made. In other words, the number of words in the dictionary
      *
-     * @param str  The string to check
-     * @return  A boolean indicating if the string has been found as a prefix
+     *
+     * @return  number of successful API calls
      */
-    public boolean isPrefix(String str) {
-        // looping through the Treeset
-        for(String s: this.fetchedWords){
-            int x = str.length();
-            if (s.length() >= x) {
+    public int getWordCount() {
+        return this.wordCount;
 
-                if (s.substring(0, x).equals(str.toLowerCase())) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
+
 
 }
